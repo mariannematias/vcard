23c12e86054cf49b3415cf86a20337e80d6d93ea
mariannematias <mariannec.matias@gmail.com>
2019-04-30 10:41:37 +0800

    add files

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+const home = require('../pages/home.json')
+
+/*  This is the home route. It renders the index.mustache page from the views directory.
+	Data is rendered using the Mustache templating engine. For more
+	information, view here: https://mustache.github.io/#demo */
+router.get('/', (req, res) => {
+	const data = {
+		page: home
+	}
+	res.render('index', data)
+})
+
+
+module.exports = router
